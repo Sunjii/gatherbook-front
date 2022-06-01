9e78ce91c03ddce19f5155126c098df3b6b3d40c
Sunji <sophieadenn@gmail.com>
2022-06-02 00:29:04 +0900

    App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import About from "./pages/About";
+import Write from "./pages/Write";
+import Test from "./pages/test";
+import Tales from "./pages/Tales";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/write" element={<Write />} />
+      <Route path="/result" element={<Result />} />
+      <Route path="/tales" element={<Tales />} />
+      <Route path="/test" element={<Test />} />
 
-export default App;
+      <Route path="/*" element={<h1>404 Error</h1>} />
+    </Routes>
+  );
+};
