b4dd8870d917a97c189210ad115da188e0d785b8
Sunji <sophieadenn@gmail.com>
2022-06-02 19:54:13 +0900

    [component] Card for Tale

--- src/components/Talecard.js
+++ src/components/Talecard.js
@@ -13,26 +13,22 @@ const TaleCard = (props) => {
     <Card className="w-96 mt-6">
       <CardHeader color="blue" className="relative h-56">
         <img
-          src="/img/blog.jpg"
+          src={props.imgurl}
           alt="img-blur-shadow"
           className="w-full h-full"
         />
       </CardHeader>
       <CardBody className="text-center">
         <Typography variant="h5" className="mb-2">
-          Cozy 5 Stars Apartment
+          {props.title}
         </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
-        </Typography>
+        <Typography>{props.text}</Typography>
       </CardBody>
       <CardFooter divider className="flex items-center justify-between py-3">
-        <Typography variant="small">$899/night</Typography>
+        <Typography variant="small">{props.id}</Typography>
         <Typography variant="small" color="grey" className="flex gap-1">
           <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-          Barcelona, Spain
+          {props.author}
         </Typography>
       </CardFooter>
     </Card>
