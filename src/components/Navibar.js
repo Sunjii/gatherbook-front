3e75b013a241a6e1a62b547531325a1e40b375ed
Sunji <sophieadenn@gmail.com>
2022-06-04 03:12:44 +0900

    [feat] responsive navibar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,4 +1,12 @@
-import { Button, Navbar, Typography } from "@material-tailwind/react";
+import {
+  Button,
+  Menu,
+  MenuHandler,
+  MenuItem,
+  MenuList,
+  Navbar,
+  Typography,
+} from "@material-tailwind/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -6,9 +14,9 @@ const Navibar = () => {
   return (
     <Navbar
       color="blue"
-      className="w-full h-14 mx-auto bg-blue-500 rounded-none"
+      className="max-w-full h-14 mx-auto lg:bg-blue-500 rounded-none"
     >
-      <div className="container flex justify-between items-center ">
+      <div className="container max-w-full flex justify-between items-center ">
         <Link
           className="mx-4 flex items-center font-bold hover:text-amber-400"
           to="/"
@@ -17,29 +25,68 @@ const Navibar = () => {
         </Link>
         <div className="flex items-center gap-6">
           <Link
-            className="mx-4 flex items-center font-bold hover:text-amber-400"
+            className="mx-4 flex items-center font-bold hover:text-amber-400 invisible lg:visible"
             to="/about"
           >
             About Us
           </Link>
           <Link
-            className="mx-4 flex items-center font-bold hover:text-amber-400"
+            className="mx-4 flex items-center font-bold hover:text-amber-400 invisible lg:visible"
             to="/write"
           >
             글쓰기
           </Link>
           <Link
-            className="mx-4 flex items-center font-bold hover:text-amber-400"
+            className="mx-4 flex items-center font-bold hover:text-amber-400 invisible lg:visible"
             to="/tales"
           >
             구경하기
           </Link>
           <a
-            className="mx-4 flex items-center font-bold hover:text-amber-400"
+            className="mx-4 flex items-center font-bold hover:text-amber-400 invisible lg:visible"
             href="https://github.com/boostcampaitech3/final-project-level3-nlp-06"
           >
             Github
           </a>
+          <Menu className="visible lg:invisible">
+            <MenuHandler>
+              <Button className="visible lg:invisible">OPEN</Button>
+            </MenuHandler>
+            <MenuList>
+              <MenuItem>
+                <Link
+                  className="mx-4 flex items-center font-bold hover:text-amber-400"
+                  to="/about"
+                >
+                  About Us
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link
+                  className="mx-4 flex items-center font-bold hover:text-amber-400 "
+                  to="/write"
+                >
+                  글쓰기
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link
+                  className="mx-4 flex items-center font-bold hover:text-amber-400 "
+                  to="/tales"
+                >
+                  구경하기
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  className="mx-4 flex items-center font-bold hover:text-amber-400 "
+                  href="https://github.com/boostcampaitech3/final-project-level3-nlp-06"
+                >
+                  Github
+                </a>
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </div>
       </div>
     </Navbar>
