a1a984f3d343410d7e6026e79e61c6354e892407
Sunji <sophieadenn@gmail.com>
2022-06-05 23:59:38 +0900

    bg-color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,20 @@ const DefaultFooter = () => {
   return (
     <>
       <footer>
-        <div className="relative bg-blue-200 pt-8 pb-6">
+        <div className="relative bg-light-blue-800 pt-8 pb-6">
           <div className="container max-w-7xl mx-auto px-4">
             <div className="flex flex-wrap text-center lg:text-left pt-6">
               <div className="w-full lg:w-6/12 px-4"></div>
             </div>
-            <hr className="my-6 border-amber-400 " />
+            <hr className="my-6 border-white " />
             <div className="flex flex-wrap items-center md:justify-between justify-center">
               <div className="w-full md:w-4/12 px-4 mx-auto text-center">
                 <div className="text-sm text-white font-medium py-1">
-                  <>{new Date().getFullYear()} Naver Boostcamp AI Tech </>
-                  <p>자연어 한접시</p>
+                  <>
+                    Copyrightⓒ {new Date().getFullYear()} Naver Boostcamp AI
+                    Tech 3rd
+                  </>
+                  <p>made by 자연어 한접시</p>
                 </div>
               </div>
             </div>
