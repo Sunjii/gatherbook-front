809f6637f4f3c2e07d16de8e9b77efafdeb52004
Sunji <sophieadenn@gmail.com>
2022-06-08 19:02:39 +0900

    web font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/pages/**/*.{html,js}", "./src/components/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        woori: ["WooridaumB"],
+      },
+      colors: {
+        myBlue: "#40a8f0",
+        myGreen: "#a14b36",
+      },
+    },
   },
   plugins: [],
 });
