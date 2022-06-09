8b5bdb689dd5da9ad33b46db10f351884ea82b19
Sunji <sophieadenn@gmail.com>
2022-06-09 23:15:35 +0900

    kakao icon

--- src/components/KakaoShare.js
+++ src/components/KakaoShare.js
@@ -56,7 +56,12 @@ const KakaoShareButton = () => {
     <div className="kakao-share-button">
       {/* Kakao share button */}
       <button id="kakao-link-btn">
-        <img src="https://i.imgur.com/j8Khf0R.png" alt="kakao-share-icon" />
+        <img
+          src="https://i.imgur.com/j8Khf0R.png"
+          height="40"
+          width="40"
+          alt="kakao-share-icon"
+        />
       </button>
     </div>
   );
